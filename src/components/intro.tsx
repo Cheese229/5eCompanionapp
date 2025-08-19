f011214e1723ca38b558cffd25ab56496c0e856e
Cheese229 <izzypepe@gmail.com>
2025-08-19 20:32:50 +1000

    temp startup intro
    quick temp startup intro to show before going to home page. will add a way to remember when its been seen before so it doesnt show up every time you open the app

---
+++ src/components/intro.tsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+
+// import images
+import intro1 from '../assets/intro/intro1.png';
+import intro2 from '../assets/intro/intro2.png';
+import intro3 from '../assets/intro/intro3.png';
+import { IonButton } from '@ionic/react';
+
+import './intro.css';
+
+interface ContainerProps {
+    onFinish: () => void;
+}
+
+const Intro: React.FC<ContainerProps> = ( {onFinish} ) => {
+
+    return (
+        <div>
+            <Swiper>
+                <SwiperSlide>
+                    <img src={intro1} alt="Intro 1" />
+                </SwiperSlide>
+                <SwiperSlide>
+                    <img src={intro2} alt="Intro 2" />
+                </SwiperSlide>
+                <SwiperSlide>
+                    <img src={intro3} alt="Intro 3" />
+                    <IonButton onClick={() => onFinish()}>Finish</IonButton>
+                </SwiperSlide>
+            </Swiper>
+        </div>
+    );
+};
+
+export default Intro;
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,5 @@
 import { IonContent, IonFooter, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import React from 'react';
+import React, { useState } from 'react';
 
 import { Card, CardContent } from "@/components/ui/card"
 import {
@@ -7,73 +7,86 @@ import {
   CarouselContent,
   CarouselItem,
 } from "@/components/ui/carousel"
+import Intro from '@/components/intro';
 
 const Home: React.FC = () => {
 
+  const [introSeen, setIntroSeen] = useState(false);
+
+  const finishIntro = async() => {
+    setIntroSeen(true);
+  }
+
   return (
-    <IonPage>
-      <IonHeader>
-        <IonToolbar>
-          <IonTitle className='pl-5'>Home</IonTitle>
-        </IonToolbar>
-      </IonHeader>
-      <IonContent className="ion-padding">
-        <section className='p-10'>
-          <div>
-            <h1 className='hover:underline max-w-min'>
-              Characters
-            </h1>
-          </div>
-          <div>
-            <Carousel className="w-full max-w-full">
-              <CarouselContent>
-                {Array.from({ length: 6 }).map((_, index) => (
-                  <CarouselItem key={index} className="md:basis-1/2 lg:basis-1/3">
-                    <div className="p-1">
-                      <Card>
-                        <CardContent className="flex aspect-square items-center justify-center p-6">
-                          <span className="text-4xl font-semibold">{index + 1}</span>
-                        </CardContent>
-                      </Card>
-                    </div>
-                  </CarouselItem>
-                ))}
-              </CarouselContent>
-            </Carousel>
-          </div>
-        </section>
-        <section className='p-10'>
-          <div>
-            <h1 className='hover:underline max-w-min'>
-              Campaigns
-            </h1>
-          </div>
-          <div>
-            <Carousel className="w-full max-w-full">
-              <CarouselContent>
-                {Array.from({ length: 2 }).map((_, index) => (
-                  <CarouselItem key={index} className="md:basis-1/2 lg:basis-1/3">
-                    <div className="p-1">
-                      <Card>
-                        <CardContent className="flex aspect-square items-center justify-center p-6">
-                          <span className="text-4xl font-semibold">{index + 1}</span>
-                        </CardContent>
-                      </Card>
-                    </div>
-                  </CarouselItem>
-                ))}
-              </CarouselContent>
-            </Carousel>
-          </div>
-        </section>
-      </IonContent>
-      {/* gonna have to figure out how to do a bottom tab navbar for mobile while having a sidebar on desktop */}
-      <IonFooter>
-        <IonToolbar className='pl-5'>
-          temp
-        </IonToolbar>
-      </IonFooter>
-    </IonPage>
+    <>
+    {!introSeen ? (
+      <Intro onFinish={finishIntro} />
+    ) : (
+      <IonPage>
+        <IonHeader>
+          <IonToolbar>
+            <IonTitle className='pl-5'>Home</IonTitle>
+          </IonToolbar>
+        </IonHeader>
+        <IonContent className="ion-padding">
+          <section className='p-10'>
+            <div>
+              <h1 className='hover:underline max-w-min'>
+                Characters
+              </h1>
+            </div>
+            <div>
+              <Carousel className="w-full max-w-full">
+                <CarouselContent>
+                  {Array.from({ length: 6 }).map((_, index) => (
+                    <CarouselItem key={index} className="md:basis-1/2 lg:basis-1/3">
+                      <div className="p-1">
+                        <Card>
+                          <CardContent className="flex aspect-square items-center justify-center p-6">
+                            <span className="text-4xl font-semibold">{index + 1}</span>
+                          </CardContent>
+                        </Card>
+                      </div>
+                    </CarouselItem>
+                  ))}
+                </CarouselContent>
+              </Carousel>
+            </div>
+          </section>
+          <section className='p-10'>
+            <div>
+              <h1 className='hover:underline max-w-min'>
+                Campaigns
+              </h1>
+            </div>
+            <div>
+              <Carousel className="w-full max-w-full">
+                <CarouselContent>
+                  {Array.from({ length: 2 }).map((_, index) => (
+                    <CarouselItem key={index} className="md:basis-1/2 lg:basis-1/3">
+                      <div className="p-1">
+                        <Card>
+                          <CardContent className="flex aspect-square items-center justify-center p-6">
+                            <span className="text-4xl font-semibold">{index + 1}</span>
+                          </CardContent>
+                        </Card>
+                      </div>
+                    </CarouselItem>
+                  ))}
+                </CarouselContent>
+              </Carousel>
+            </div>
+          </section>
+        </IonContent>
+        {/* gonna have to figure out how to do a bottom tab navbar for mobile while having a sidebar on desktop */}
+        <IonFooter>
+          <IonToolbar className='pl-5'>
+            temp
+          </IonToolbar>
+        </IonFooter>
+      </IonPage>
+    )}
+    </>
   );
 };
 
